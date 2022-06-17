b5cc20394138e3189233472a62d74d4a6bdabbb4
khairulakmal16 <khairulakmal0@gmail.com>
2022-06-17 15:19:24 +0800

    separate navbar coding

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+	<head>
+		<title>Home</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="style/navbar.css">
+		<style>
+			body {
+				margin: 0;
+				font-family:Arial;
+				text-align:center;
+				background-image:url(img/floor-tile.png);
+				background-position:center-center;
+			}
+			.table{
+				margin-top:8%;
+				width:100%;
+				border-collapse:collapse;
+				border:0;
+				text-align:center;
+				background-color:52796f;
+				color:white;
+			}
+
+			.heading{
+				font-family: Verdana;
+				text-align:center;
+				font-size: 50px;
+				margin-top:5%;
+			}
+
+		</style>
+	</head>
+	<body>
+	<?php include 'navbar.php';?>
+		<div class="heading">
+				<h1>Hi, I'm <span style="background-color: 52796f">AIMAN KAMAL.</span></h1>
+				<h6><u>Welcome to My Website.</u></h6>
+		</div>
+		<div align="center">
+			<table class="table">
+				<tr>
+					<td style="width:350px; height:350px;"><img src="img/Manparang.png" style="width:360px; height:360px; margin:0;"/></td>
+					<td>
+						<span style="font-family: roboto; font-size:25px; font-style:italic;">
+							<p>I'm currently 22 years old and part 5 computer science student at UTM Puncak Alam.</p>
+							<p>This website was developed by me to convey my personal information such as working experiences, skills and etc.</p>
+						</span>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,31 @@
+
+<html>
+  <head>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="style/navbar.css">
+  </head>
+
+  <body>
+    <div class="topnav">
+      <div class="page">
+        <a href="index.php">Home</a>
+      </div>
+
+      <div class="dropdown">
+        <button class="dropbtn">About Me</button>
+        <div class="dropdown-content">
+          <a href="personalInfo.html">About Me</a>
+          <a href="favSong.html">Favourite Songs </a>
+        </div>
+      </div>
+      <div class="page">
+        <a href="gallery.html">Gallery</a>
+        <a href="feedback.html">Feedback</a>
+      </div>
+      <a href="https://www.instagram.com/khairulxakmal/" target="_blank"><img src="img/ig.png" title="Instagram" class="social"/></a>
+      <a href="https://www.twitter.com/actuallymamal" target="_blank"><img src="img/twitter.png" title="Twitter" class="social"/></a>
+      <a href="https://www.facebook.com//" target="_blank"><img src="img/fb.jpg" title="Facebook" class="social"/></a>
+      <a href="https://github.com/khairulakmal16" target="_blank"><img src="img/github.png" title="Github" class="social"/></a>
+    </div>
+  </body>
+</html>
